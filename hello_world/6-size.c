f986948723e6a1570d50065043f25e379fb160e3
angel bermudez <9015@holbertonstudents.com>
2024-02-19 19:10:06 -0400

    090909

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,15 +1,16 @@
-#include <stdio.h>
-int main(void)
+#include<stdio.h>
 /**
- *main- prints values of datatypes in bytes
- *return: 0 when successful
- */
+*main - Prints the sizes of datatypes in bytes.
+*if succesful
+*Return: 0
+*/
+int main(void)
 {
-printf("Size of a char: %d byte(s)\n"), (unsigned long) sizeof(char);
-printf("Size of an int: %d byte(s)\n"), (unsigned long) sizeof(int);
-printf("Size of a long int: %d byte(s)\n"), (unsigned long) sizeof(long);
-printf("Size of a long long int: %d byte(s)\n"), (unsigned long) sizeof(long long int);
-printf("Size of a float: %d byte(s)\n"), (unsigned long) sizeof(float);
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
 return (0);
- 
+
 }
