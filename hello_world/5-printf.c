d26bfdee7e85d3d030ccd7f7ac1c2f46211e3d08
angel bermudez <9015@holbertonstudents.com>
2024-02-19 15:04:36 -0400

    05t04

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,11 @@
-include <stdio.h>
+include < stdio.h >
+/**<main> function
+ * <return> return value
+*/
 int main(void)
+
 {
-    printf("with proper grammar, but the outcome is a piece of art,");
-    return (0)
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
 }
+
