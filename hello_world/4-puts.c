55d6fae6c2c8fa42e74c5ce044956be59deb02be
angel bermudez <9015@holbertonstudents.com>
2024-02-19 15:28:17 -0400

    NDmiGE

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
+
 /**
- * main - print string
- * return - value of 0
+ *main- print string
+ *Return: - value of 0
 */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
-
