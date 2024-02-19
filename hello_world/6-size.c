e7681ad0763a1d611aebff3341885c2e007df627
angel bermudez <9015@holbertonstudents.com>
2024-02-19 16:32:47 -0400

    09090

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,11 +7,11 @@ long longType;
 long long longlongType;
 char charType;
 
-printf("Size of a char: %zu byte\n"), (unsigned long) sizeof(charType));
-printf("Size of an int: %zu bytes\n"), (unsigned long) sizeof(intType));
-printf("Size of a long long: %zu bytes\n"), (unsigned long) sizeof(longlongType));
-printf("Size of a long: %zu bytes\n"), (unsigned long) sizeof(longType));
-printf("Size of a float: %zu bytes\n"), (unsigned long) sizeof(floatType));
+printf("Size of a char: %zu byte\n"), (unsigned long) sizeof(charType);
+printf("Size of an int: %zu bytes\n"), (unsigned long) sizeof(intType);
+printf("Size of a long long: %zu bytes\n"), (unsigned long) sizeof(longlongType);
+printf("Size of a long: %zu bytes\n"), (unsigned long) sizeof(longType);
+printf("Size of a float: %zu bytes\n"), (unsigned long) sizeof(floatType);
 return (0);
 }
 
