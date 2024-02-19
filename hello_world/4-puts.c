08bc4bf2428991c6f47aab860a0c31c0c41d71ba
angel bermudez <9015@holbertonstudents.com>
2024-02-19 14:14:19 -0400

    4t2

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-int < main > void
+int main (void)
 {
-char mystr[] = "Programming is like building a multilingual puzzle"
-  puts(mystr);
- return (0);
+  puts("\"Programming is like a building a multilingual puzzle")
+  return(0)
 }
