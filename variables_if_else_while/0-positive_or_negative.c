90143dbe35be014981fc53661b723a9455ef699d
angel bermudez <9015@holbertonstudents.com>
2024-02-20 13:49:03 -0400

    0t04

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 
 /* betty style doc for function main goes there */
 /**
- * main- entry point
+ * main - entry point
  * Desc: print if value of n is either positive ngative or equal to zero.
  * return: If (success) always return 0.
 */
@@ -15,13 +15,10 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/*print if n = zero*/
 	if (n == 0)
 		printf("%d is zero\n", n);
-		/*print if n > 0*/
 	if (n > 0)
 		printf("%d is positive\n", n);
-		/*print if n < 0*/
 	if (n < 0)
 		printf("%d is negative\n", n);
 	return (0);
