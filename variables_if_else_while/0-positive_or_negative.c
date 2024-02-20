1225ecd44f39d68981c45d6a6d27acca5e2ebfe2
angel bermudez <9015@holbertonstudents.com>
2024-02-20 13:40:37 -0400

    0t1

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -18,10 +18,10 @@ int main(void)
 		printf("%d is zero\n", n);
 		/*print if n > 0*/
 	if (n > 0)
-		printf("%d is negative\n", n);
+		printf("%d is positive\n", n);
 		/*print if n < 0*/
 	if (n < 0)
-		printf("%d is positive\n", n);
+		printf("%d is negative\n", n);
 	return (0);
 }
 
