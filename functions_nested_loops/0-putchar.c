c384cb5058ecd78e94f1d5b7d3acf37b359dda44
angel bermudez <9015@holbertonstudents.com>
2024-02-21 10:59:13 -0400

    0t01

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
-
 /**
- * main - functions prints _putchar
- * Return: always 0 (Success)
+* altprint- altprint(void): prints defined string
+* Return: always 0 (Success)
 */
 int altprint(void)
 {
 printf("_putchar\n");
 }
+
+/**
+* main - entry point
+* Return: always 0 if (success)
+*/
 int main(void)
 {
 altprint();
