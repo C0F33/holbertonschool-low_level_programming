efe3975609ba38c274d79fd6445cfb955923bb29
angel bermudez <9015@holbertonstudents.com>
2024-02-20 21:29:11 -0400

    06t0

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main - acces point
+*Description: print numbers 0 to 9
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int c;
+for (c = 0; c <= 9; c++)
+
+putchar("%d", c);
+putchar("\n");
+return (0);
+}
