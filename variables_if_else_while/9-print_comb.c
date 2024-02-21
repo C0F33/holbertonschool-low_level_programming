98e528ca718c1c997de1dc403a8b147cd389298f
angel bermudez <9015@holbertonstudents.com>
2024-02-21 00:03:05 -0400

    090909090

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -11,11 +11,11 @@ int c;
 for (c = 0; c <= 9; c++)
 {
 putchar((c % 10) + '0');
-    if (c == 9)
-    continue;
-    putchar(',');
-    putchar(' ');
+if (c == 9)
+continue;
+putchar(',');
+putchar(' ');
 }
-    putchar('$');
+putchar('$');
 return (0);
 }
