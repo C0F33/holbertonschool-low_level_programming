70a46dbc3e68b051b94f84a5ffe0cddd02dfb22f
angel bermudez <9015@holbertonstudents.com>
2024-02-20 23:59:59 -0400

    900

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main - acces point
+*Description: print base 16 numbers
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int c;
+for (c = 0; c <= 9; c++)
+{
+putchar((c % 10) + '0');
+    if (c == 9)
+    continue;
+    putchar(',');
+    putchar(' ');
+}
+    putchar('$');
+return (0);
+}
