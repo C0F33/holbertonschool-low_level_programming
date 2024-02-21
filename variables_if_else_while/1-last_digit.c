37115b471bbb4a0280f5064dc0099a039cb42c82
angel bermudez <9015@holbertonstudents.com>
2024-02-20 20:19:50 -0400

    1t0989

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,9 +2,10 @@
 #include <time.h>
 #include <stdio.h>
 /**
-*main - Entry point.
-*Desc: Print evaluation of value of n.
-*Return: 0 if (success)
+ *
+main - print the last digit of the number stored in the variable
+Description: print the last digit of the number stored in the variable
+Return: Always 0 (Success)
 */
 int main(void)
 {
@@ -12,10 +13,10 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n % 10 > 5)
-printf("The last digit of %d is %d and is greater then 5\n", n, n % 10);
-if (n % 10 == 0)
-printf("The last digit of %d is %d and is 0\n", n, n % 10);
-else(n % 10 < 6 && n % 10 != 0);
-printf("The last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+else if (n % 10 == 0)
+printf("Last digit of %d is %d and is 0\n", n, n % 10);
+else if (n % 10 < 6 && n % 10 != 0)
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 return (0);
 }
---
+++ variables_if_else_while/1-last_digit1.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point.
+*Desc: Print evaluation of value of n.
+*Return: 0 if (success)
+*/
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n % 10 > 5)
+printf("The last digit of %d is %d and is greater then 5\n", n, n % 10);
+if (n % 10 == 0)
+printf("The last digit of %d is %d and is 0\n", n, n % 10);
+else(n % 10 < 6 && n % 10 != 0);
+printf("The last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+return (0);
+}
