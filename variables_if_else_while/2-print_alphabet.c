9059348128ecd406e1824100764ed92873157dd1
angel bermudez <9015@holbertonstudents.com>
2024-02-20 20:32:39 -0400

    2t01

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main - acces poitn
+*Description: print the alphabet
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
