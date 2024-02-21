a9934c4c9a0ca1432e4a315ec3323e6282b4891e
angel bermudez <9015@holbertonstudents.com>
2024-02-20 21:03:38 -0400

    5t0

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main - acces point
+*Description: print numbers 0 to 9
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int c;
+for (c = 0; c <= 9; c++)
+
+printf("%d", c);
+printf("\n");
+return (0);
+}
