22e340583251b49f7239a36b65ad1906dc9f7e2d
angel bermudez <9015@holbertonstudents.com>
2024-02-22 23:49:51 -0400

    090ll

---
+++ functions_nested_loops/mult.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * mutl - multiplies two variables
+ * Return: returns result of a * b
+*/
+char mult(char a, char b)
+{
+    char result =(a * b)
+    return = (result);
+}
