d252ea7424e5f0977261ec8a9ba4d71b20194e31
angel bermudez <9015@holbertonstudents.com>
2024-02-25 15:10:51 -0400

    10t01

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - Adds two variables
+ * Return: Returns summ of variables.
+ * @a: Variable to be summ.
+ * @b: Variable to be summ.
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
