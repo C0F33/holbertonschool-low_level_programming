22da99eeabf0b8cb000c1d5391d3ffa307b08437
angel bermudez <9015@holbertonstudents.com>
2024-02-26 14:05:15 -0400

    02t00

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/*
+ *_strlen- check the code
+ *@s: string to be analized.
+ * Return: Always 0.
+ */
+int _strlen(const char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+(i++);
+return (0);
+}
