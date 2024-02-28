8b42af70146fcd99b3266c91efb68243c7eecf43
angel bermudez <9015@holbertonstudents.com>
2024-02-28 16:27:41 -0400

    001t095

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -8,18 +8,18 @@
 */
 char *_strncat(char *dest, const char *src, int n)
 {
-char *start = dest;
+char *start = *dest;
 while (*dest != '\0')
 {
-dest++;
+*dest++;
 }
 while (*src != '\0' && n > 0)
 {
 *dest = *src;
-dest++;
-src++;
+*dest++;
+*src++;
 n--;
 }
 *dest = '\0';
-return (start);
+return (*start);
 }
