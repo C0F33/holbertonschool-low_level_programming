8cb580272ea8201633234a5b34aa15b081ecd99b
angel bermudez <9015@holbertonstudents.com>
2024-02-28 16:25:04 -0400

    001t210

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,25 +1,25 @@
 #include "main.h"
 /**
- * _strncat - 
+ * _strncat -
  * Return: pointer positions start of string
- * @dest:
- * @src:
- * @n:
+ * @dest: location to be copied
+ * @src: script to be copied
+ * @n: byte limits
 */
-char *_strncat(char *dest, const char *src, int n) {
-    char *start = dest;
-
-    while (*dest != '\0') {
-        dest++;
-    }
-
-    while (*src != '\0' && n > 0) {
-        *dest = *src;
-        dest++;
-        src++;
-        n--;
-    }
-    *dest = '\0';
-
-    return start;
+char *_strncat(char *dest, const char *src, int n)
+{
+char *start = dest;
+while (*dest != '\0')
+{
+dest++;
+}
+while (*src != '\0' && n > 0)
+{
+*dest = *src;
+dest++;
+src++;
+n--;
+}
+*dest = '\0';
+return (start);
 }
