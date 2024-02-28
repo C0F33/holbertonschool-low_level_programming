86bb152a582933cb9486913d64a8b351cab2f30d
angel bermudez <9015@holbertonstudents.com>
2024-02-28 16:36:49 -0400

    004t00

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,25 +1,26 @@
 #include "main.h"
+
 /**
- * _strncat -
- * Return: pointer positions start of string
- * @dest: location to be copied
- * @src: script to be copied
- * @n: byte limits
-*/
-char *_strncat(char *dest, const char *src, int n)
+ * _strncat - concatenates two strings, limiting the number of bytes
+ * @dest: destination string
+ * @src: source string
+ * @n: maximum number of bytes to be copied from src
+ * Return: pointer to the resulting string
+ */
+char *_strncat(char *dest, char *src, int n)
 {
-char *start = *dest;
+char *start = dest;
 while (*dest != '\0')
 {
-*dest++;
+dest++;
 }
 while (*src != '\0' && n > 0)
 {
 *dest = *src;
-*dest++;
-*src++;
+dest++;
+src++;
 n--;
 }
 *dest = '\0';
-return (*start);
+return (start);
 }
