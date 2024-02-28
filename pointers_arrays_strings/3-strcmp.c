4c127447f6df2ae537112738f270847e2a92f9b7
angel bermudez <9015@holbertonstudents.com>
2024-02-28 17:09:59 -0400

    003t001

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _strcmp - compares 2 strings by subtractig the amount of bytes in give arrays.
+* @s1: string to be compared 1
+* @s2: string to be compared 2
+* Return: subtracted amount of space between s1 and s2.
+*/
+int _strcmp(char *s1, char *s2)
+{
+while (*s1 && (*s1 == *s2))
+{
+s1++;
+s2++;
+}
+return *(unsigned char *)s1 - *(unsigned char *)s2;
+}
