c1d2bc5d140d3cb557f6b67b41061e50e0775168
angel bermudez <9015@holbertonstudents.com>
2024-02-28 23:12:10 -0400

    0007

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,18 @@
+ #include "main.h"
+ /**
+  *cap_string - foobar
+  * Retun: strn back to user
+ */
+char *cap_string(char *str)
+{
+int i;
+while (str[i] != '\0')
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - ('a' - 'A');
+}
+i++;
+}
+return (str);
+}
