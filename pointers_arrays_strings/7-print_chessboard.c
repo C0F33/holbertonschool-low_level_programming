5b6e5ffb433d0a729df9c621b93ca4c0b4e08b26
angel bermudez <9015@holbertonstudents.com>
2024-02-29 22:53:40 -0400

    090909

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
  * print_chessboard - function that prints the chessboard
- * @a: array of chess pieces
+ * @board: array of chess pieces
  * Return: void
  */
-  void print_chessboard(char (*board)[8])
+void print_chessboard(char (*board)[8])
 {
 int i, j;
 for (i = 0; i < 8; i++)
