d283b05dc3daa525e4b86902524b629bc0551af6
angel bermudez <9015@holbertonstudents.com>
2024-02-29 22:57:28 -0400

    0756

--- pointers_arrays_strings/6-print_chessboard.c
+++ pointers_arrays_strings/6-print_chessboard.c
@@ -4,14 +4,14 @@
  * @a: array of chess pieces
  * Return: void
  */
-  void print_chessboard(char (*board)[8])
+  void print_chessboard(char (*a)[8])
 {
 int i, j;
 for (i = 0; i < 8; i++)
 {
 for (j = 0; j < 8; j++)
 {
-_putchar(board[i][j]);
+_putchar(a[i][j]);
 _putchar(' ');
 }
 _putchar('\n');
