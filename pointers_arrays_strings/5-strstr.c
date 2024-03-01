86dcefecb1d4222a1bc1edd9dde49568d9e20896
angel bermudez <9015@holbertonstudents.com>
2024-02-29 22:18:58 -0400

    05t009

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strstr- locates a substring in string s
+ * @haystack: string to be searched
+ * @needle: string to be found
+ * Return: pointer to needle in haystack or null
+*/
+char *_strstr(char *haystack, char *needle)
+{
+int *NULL = '\0';
+if (*needle == '\0')
+return (char *)haystack;
+while (*haystack != '\0')
+{
+const char *pHaystack = haystack;
+const char *pNeedle = needle;
+while (*pNeedle != '\0' && *pHaystack == *pNeedle) {
+pHaystack++;
+pNeedle++;
+}
+if (*pNeedle == '\0')
+return (char *)haystack;
+haystack++;
+}
+return NULL;
+}
