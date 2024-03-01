c26dea56ee1fb040b8908e2e70b4a257375fe9f3
angel bermudez <9015@holbertonstudents.com>
2024-02-29 21:01:42 -0400

    02t00

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr- locates char in string
+ * @s: pointer to string to be analized
+ * @c: character to be found
+ * Return: char pointer if succesful else null
+*/
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+{
+return ((char *)s);
+s++;
+}
+}
+return ('\0');
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -31,3 +31,4 @@ void puts_half(char *str);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_memset(char *s, char b, unsigned int n);
+char *_strchr(char *s, char c);
