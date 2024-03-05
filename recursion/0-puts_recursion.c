686133e1cb1f108d3dd2733305140fd2b33326eb
angel bermudez <9015@holbertonstudents.com>
2024-03-04 21:19:04 -0400

    78656

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *_puts_recursion - print a string with recursion
- *@s: string to be printed
- */
+*_puts_recursion - print a string with recursion
+*@s: string to be printed
+*/
 void _puts_recursion(char *s)
 {
 if (*s == '\0')
