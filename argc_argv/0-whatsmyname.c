e4a6982ff92b7801f570be646c4369641607722a
angel bermudez <9015@holbertonstudents.com>
2024-03-05 11:44:43 -0400

    0978

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - funcion main
+ * @argc: number of commands
+ * @argv: strings
+ * Return: number of arguments
+*/
+int main(int argc, char *argv[])
+{
+    printf("%s\n", argv[0]);
+    return (0);
+}
