7c1bf9fd2f7821919c5a38233afe3a4cd94f44eb
angel bermudez <9015@holbertonstudents.com>
2024-03-05 17:16:53 -0400

    03t01

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -11,15 +11,14 @@ int main(int argc, char *argv[])
 int a;
 int b;
 int product;
-if (argc > 3)
-{
 a = atoi(argv[1]);
 b = atoi(argv[2]);
 product = (a * b);
 printf("%d\n", product);
 return (0);
+if (argc != 3)
+{
+        printf("Error\n");
+        return 1;
 }
-else
-printf ("Error\n");
-return(1);
 }
