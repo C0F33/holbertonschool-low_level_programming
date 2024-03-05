105d5bbb371c87f852e66f46ed9d437e04f01871
angel bermudez <9015@holbertonstudents.com>
2024-03-05 15:31:59 -0400

    0t0o

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -7,8 +7,9 @@
  */
 int main(int argc, char *argv[])
 {
-	while (--argc >= 0) {
-        printf("%d\n", argc);
-    }
-    return 0;
+while (--argc >= 0) {
+printf("%d\n", argc);
 }
+return 0;
+}
+
