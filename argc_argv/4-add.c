415c5f538501acab7a0deddc9eef112767e12e25
angel bermudez <9015@holbertonstudents.com>
2024-03-05 21:58:43 -0400

    009t007

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,16 +1,24 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
+/**
+ * main- add ints in argc
+ * @argc: nums to be added
+ * @argv: Individual values
+ * Return: 0 if success
+*/
 int main(int argc, char *argv[])
 {
 int i;
 int sum = 0;
-if (argc > 1) {
-for(i = 1; i < argc; i++) {
-printf("argv[%d] = %s\n", i, argv[i]);
-if (atoi(argv[i]) == 0 && *argv[i] != '0') {
-printf("Error: Non-integer argument detected\n");
-return 1;
+if (argc > 1)
+{
+for (i = 1; i < argc; i++)
+{
+if (atoi(argv[i]) == 0 && *argv[i] != '0')
+{
+printf("Error\n");
+return (1);
 }
 sum += atoi(argv[i]);
 }
@@ -18,8 +26,8 @@ sum += atoi(argv[i]);
 else
 {
 printf("0\n");
-return 1;
+return (1);
 }
-printf("Sum: %d\n", sum);
-return 0;
+printf("%d\n", sum);
+return (0);
 }
