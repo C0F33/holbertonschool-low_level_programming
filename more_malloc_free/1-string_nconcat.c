4e872afdaf772f50d9616c4e44897332982193e9
angel bermudez <9015@holbertonstudents.com>
2024-03-07 20:09:36 -0400

    01t003

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -9,10 +9,10 @@ size_t len2 = (s2 != NULL) ? strlen(s2) : 0;
 size_t actual_n = (n >= len2) ? len2 : n;
 char *s3 = malloc(len1 + actual_n + 1);
 if (!s3)
-return NULL;
+return (NULL);
 if (s1 != NULL)
 strncpy(s3, s1, len1);
 strncat(s3 + len1, s2, actual_n);
 s3[len1 + actual_n] = '\0';
-return s3;
+return (s3);
 }
