34500fbaa094d0795214890fd80804025f4033b8
angel bermudez <9015@holbertonstudents.com>
2024-03-11 18:31:01 -0400

    00t02

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,21 @@
-typedef struct{
-    char name[10];
-    float age;
-    char owner[10];
-} dog;
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - dog description
+ * @name:name of the dog
+ * @age:age of the dog
+ * @owner: owner of the dog
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
+#endif
