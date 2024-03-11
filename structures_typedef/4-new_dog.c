6ec19e74452cc624b8d18980173bc4f475160489
angel bermudez <9015@holbertonstudents.com>
2024-03-11 19:53:10 -0400

    04003

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,32 +1,44 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 /**
- * new_dog- creates space for a new dog
- * @name: string
- * @age: int
- * @owner: name of owner
- * Return: pointer to new dog
- */
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: struct dog
+ * if it fails, returns NULL
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *p_dog;
-if (!name || !owner)
-return (NULL);
-p_dog = malloc(sizeof(dog_t));
-if (!p_dog)
-return (NULL);
-
-p_dog->name = strdup(name);
-p_dog->owner = strdup(owner);
-if (!p_dog->name || !p_dog->owner)
+int i, lname, lowner;
+p_dog = malloc(sizeof(*p_dog));
+if (p_dog == NULL || !(name) || !(owner))
 {
-free(p_dog->name);
-free(p_dog->owner);
 free(p_dog);
 return (NULL);
 }
+for (lname = 0; name[lname]; lname++)
+;
+for (lowner = 0; owner[lowner]; lowner++)
+;
+p_dog->name = malloc(lname + 1);
+p_dog->owner = malloc(lowner + 1);
+if (!(p_dog->name) || !(p_dog->owner))
+{
+free(p_dog->owner);
+free(p_dog->name);
+free(p_dog);
+return (NULL);
+}
+for (i = 0; i < lname; i++)
+p_dog->name[i] = name[i];
+p_dog->name[i] = '\0';
 p_dog->age = age;
+for (i = 0; i < lowner; i++)
+p_dog->owner[i] = owner[i];
+p_dog->owner[i] = '\0';
 return (p_dog);
 }
