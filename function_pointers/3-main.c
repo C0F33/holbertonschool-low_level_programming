4fb6c126b963450dd083a8f4e99e99984f28511f
angel bermudez <9015@holbertonstudents.com>
2024-03-13 15:44:14 -0400

    03t01

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -5,18 +5,18 @@
 
 int main(int argc, char *argv[])
 {
-    int result;
-    int a;
-    int b;
-    char *c;
-
-    if (argc < 4)
-    {
-       a = atoi(argv[1]);
-       b = atoi(argv[3]);
-       c = argv[2];
-    }
-    int (*f)(int,int) = get_op_func(c);
-    result = f(a,b);
+int result;
+int a;
+int b;
+char *c;
+a = atoi(argv[1]);
+b = atoi(argv[3]);
+c = argv[2];
+if (argc != 4)
+{
+printf("Error\n");
+return (98);
+}
+result = f(a,b);  
 return (result);  
 }
