3f9fa9356770352a46a8860d91bd7b7cd88b4d2c
angel bermudez <9015@holbertonstudents.com>
2024-03-25 20:42:54 -0400

    00t003

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,20 +3,16 @@
 
 size_t print_list(const list_t *h)
 {
-    const list_t *ptr = h;
-    size_t i = 0;
-    
-    if (ptr == NULL)
-    {
-        printf("[0] (nil)\n");
-        return (1);
-    }
-    
-    while (ptr != NULL)
-    {
-        printf("[%lu] %s\n", i, ptr -> str);
-        ptr = ptr->next;
-        i++;
-    }
-    return (i);
+const list_t *ptr = h;
+size_t i = 0;
+while (ptr != NULL)
+{
+if (ptr->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", ptr->len, ptr->str);
+ptr = ptr->next;
+i++;
+}
+return (i);
 }
